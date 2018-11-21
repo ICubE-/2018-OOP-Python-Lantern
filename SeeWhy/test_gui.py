8b6f9774306cf04eb68a29b895d4f164bdd8307d
See-Y <jyw10234@sasa.hs.kr>
2018-11-22 07:07:48 +0900

    new

--- SeeWhy/test_gui.py
+++ SeeWhy/test_gui.py
@@ -1,12 +1,15 @@
 import sys
-from PyQt5.QtWidgets import QApplication, Qwidget
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
 
-class Exam(Qwidget):
+class Exam(QWidget):
     def __init__(self):
         super().__init__()
         self.initUI()
 
     def initUI(self):
+        btn=QPushButton('Click', self)
+        btn.resize(btn.sizeHint())#sizeHint : change by letters
+        btn.move(20, 30)
         self.show()
 
 app = QApplication(sys.argv)
