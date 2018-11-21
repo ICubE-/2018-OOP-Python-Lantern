0f784997ddf5ddaa798dc621f534781425385d67
See-Y <jyw10234@sasa.hs.kr>
2018-11-22 07:26:30 +0900

    adding

--- SeeWhy/test_gui.py
+++ SeeWhy/test_gui.py
@@ -7,9 +7,15 @@ class Exam(QWidget):
         self.initUI()
 
     def initUI(self):
+        #create button
         btn=QPushButton('Click', self)
         btn.resize(btn.sizeHint())#sizeHint : change by letters
-        btn.move(20, 30)
+        btn.setToolTip('툴팁입니다.<br>Hello</br>')#when you put your mouse on the button
+        btn.move(20, 30)#set button location
+
+        self.setGeometry(300,300,400,500)#set window size
+        self.setWindowTitle('Floweytale')#set window name
+
         self.show()
 
 app = QApplication(sys.argv)
