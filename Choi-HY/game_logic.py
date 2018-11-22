4e7a0e187f65c3f593fd50f379da9d906fa5ae7d
Choi-HY <bayernmunchen1213@sasa.hs.kr>
2018-11-22 17:44:46 +0900

    Update game_logic.py

--- Choi-HY/game_logic.py
+++ Choi-HY/game_logic.py
@@ -1,5 +1,15 @@
+import operator
+
 class player():
-    card_list=list()
     def __init__(self, nickname):
         self.nickname=nickname
-    def put_card(self, ):
+        self.card_list=list(range(1,8))
+        self.time_dict={'공강':1, '자습시간':1, '취침시간':1}
+    def put_card(self, card_num):
+        try:
+            self.card_list.remove(card_num)
+        except ValueError:
+            print("Choose From What You Have")
+    def get_time(self, time, amount):
+        self.time_dict[time]=self.time_dict[time]+amount
+        self.time_dict=dict(sorted(self.time_dict.items(), key=operator.itemgetter(1), reverse=True))
