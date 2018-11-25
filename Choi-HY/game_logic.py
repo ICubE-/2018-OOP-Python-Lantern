8c727dffaf8165574a4aa2af269cf79543eed5c1
Choi-HY <bayernmunchen1213@sasa.hs.kr>
2018-11-25 17:29:22 +0900

    Update game_logic.py

--- Choi-HY/game_logic.py
+++ Choi-HY/game_logic.py
@@ -1,5 +1,3 @@
-import operator
-
 class player():
     def __init__(self, nickname):
         self.nickname=nickname
@@ -12,4 +10,17 @@ class player():
             print("Choose From What You Have")
     def get_time(self, time, amount):
         self.time_dict[time]=self.time_dict[time]+amount
-        self.time_dict=dict(sorted(self.time_dict.items(), key=operator.itemgetter(1), reverse=True))
+    def take_time(self):
+        waste=max(self.time_dict.items(), key=lambda x:x[1])[0]
+        waste_time=max(self.time_dict.items(), key=lambda x:x[1])[1]
+        self.time_dict[waste]=0
+        return waste_time
+
+num_player=int(input())
+nick_player=input().split(' ')
+player_list=[]
+for i in num_player:
+    player_list.append(player(nick_player[i]))
+
+
+
