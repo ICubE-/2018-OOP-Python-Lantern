dd139a639b8e48c3a6faa1a22d1a8ec5f79dd0e3
Choi-HY <bayernmunchen1213@sasa.hs.kr>
2018-11-27 22:46:00 +0900

    Update game_logic.py

--- Choi-HY/game_logic.py
+++ Choi-HY/game_logic.py
@@ -10,6 +10,8 @@ class player():
             self.card_list.remove(card_num)
         except ValueError:
             print("Choose From What You Have")
+        else:
+            self.card_list.sort()
     def get_time(self, time, amount):
         self.time_dict[time]=self.time_dict[time]+amount
     def take_time(self):
@@ -18,30 +20,42 @@ class player():
         self.time_dict[waste]=0
         return waste_time
 
+class time_monster():
+    def __init__(self, name):
+        self.name=name
+        self.reward=[]
+    def get_reward(self, reward):
+        self.reward.append(reward)
 
 def get_input(play):
     tmp=int(input())
     return (tmp, player_list[play])
 
-def run_game(num, tot_time):
-    input_list={}
-    for i in range(num):
-        while True:
-            tmp=get_input(i)
-            if tmp[0] in player_list[i].card_list:
-                input_list[tmp[0]]=tmp[1]
-                break
-            else:
-                continue
-    for i in range(num):
-        for j in range(num):
-            if input_list[i]==input_list[j] and i!=j:
-                input_list[i]=input_list[j]=0
-    tot=0
-    for i in range(num):
-        tot=tot+input_list[i]
-    if tot_time<=tot:
-        print("Success")
+def run_game(num, round_num):
+    while True:
+        tmp=random.randint(8)
+        time_now=time_list[tmp]
+        time_list.pop(tmp)
+        input_list={}
+        for i in range(num):
+            while True:
+                tmp=get_input(i)
+                if tmp[0] in player_list[i].card_list:
+                    input_list[tmp[0]]=tmp[1]
+                    break
+                else:
+                    continue
+        for i in range(num):
+            for j in range(num):
+                if input_list[i]==input_list[j] and i!=j:
+                    input_list[i]=input_list[j]=0
+        tot=0
+        for i in range(num):
+            tot=tot+input_list[i]
+        if time_now<=tot:
+            print("Success")
+        else:
+            print("Failed")
 
 
 
@@ -49,11 +63,12 @@ num_player=int(input())
 nick_player=input().split(' ')
 player_list=[]
 time_list=[]
+for i in range(8):
+    time_list.append()
 for i in range(num_player):
     player_list.append(player(nick_player[i]))
-global player_list
-while True:
-    tmp=random.randint(8)
+global player_list, time_list
+for i in range(5):
+    run_game(num_player, i)
 
-    run_game(num_player)
 
