5c98414543f23d2f58926699eb34583643b955de
Choi-HY <bayernmunchen1213@sasa.hs.kr>
2018-11-29 03:23:35 +0900

    Update game_logic.py

--- Choi-HY/game_logic.py
+++ Choi-HY/game_logic.py
@@ -17,7 +17,7 @@ class player():
             self.card_list.sort()
             return True
 
-    def get_reward(self, time, amount):
+    def get_reward(self,  amount):
         ind=0
         for i in amount:
             self.reward_dict[reward_name[ind]]=self.reward_dict[reward_name[ind]]+i
@@ -29,6 +29,9 @@ class player():
 
     def init_time(self):
         self.card_list=list(range(1,8))
+
+    def init_reward(self):
+        self.reward_dict={'자유로운 공강':1, '행복한 취미생활':1, '편안한 숙면':1}
 
 class time_monster():
     def __init__(self, name, reward, hp):
@@ -42,12 +45,12 @@ class time_monster():
 def get_input(play):
     global player_list
     tmp=int(input())
-    return (tmp, player_list[play])
+    return [tmp, player_list[play]]
 
 def run_game(num, round_num, task_list):
     for i in range(round_num):
-        tmp=random.randint(8)
-        time_now=task_list[tmp]
+        tmp=random.randint(1, 5)
+        task_now=task_list[tmp]
         task_list.pop(tmp)
 
         input_list=[]
@@ -73,28 +76,45 @@ def run_game(num, round_num, task_list):
         for i in range(num):
             tot=tot+input_list_new[i][0]
 
-        if time_now.hp<=tot:
+        if task_now.hp<=tot:
             print("Success")
+            for i in range(3):
+                max=-1
+                for j in input_list_new:
+                    if j[0]>max:
+                        max=j[0]
+                for j in input_list_new:
+                    if j[0]==max:
+                        j[1].get_reward(task_now.reward[i])
+                        input_list_new.remove(j)
 
         else:
             print("Failed")
+            min=99999
+            for i in input_list:
+                if i[0]<min:
+                    min=i[0]
+            for i in input_list:
+                if i[0]==min:
+                    i[1].take_time()
+
 
 def play_again():
     return input('Press Y to restart').lower().startswith('y')
 
 
 num_player=int(input())
-nick_player=input().split(' ')
+nick_player=tuple(input().split(' '))
 player_list=[]
 
 task_name1=['KYPT', '세종수학축전', '한화사이언스챌린지', '해커톤', '동아리 발표 대회']
-task_name2=['융합@수학 산출물', 'SA', '객체지향프로그래밍 프로젝트', '도시환경과 도시계획 연구', '논리적 글쓰기 인문학보고서']
+task_name2=['융합@수학 산출물', '삼성 휴먼테크 논문대상', '객체지향프로그래밍 프로젝트', '도시환경과 도시계획 연구', '논리적 글쓰기 인문학보고서']
 task_name3=['의자 만들기', '스파게티 다리 만들기', '교류 발전기 만들기', '논문 분석', '창작 시 콘서트']
 task_name_all=[task_name1, task_name2, task_name3]
 task_header=['외부활동', '연구활동', '조별과제']
 task_list={}
 
-reward_list=[[[], [], []], [[], [], []], [[], [], []], [[], [], []], [[], [], []]]
+reward_list=[[[3, 1, 2], [1, 1, 2], [0, 1, 0]], [[3, 0, 0], [0, 2, 0], [0, 0, 1]], [[2, 1, 2], [1, 0, 1], [0, 1, 0]], [[2, 2, 2], [1, 1, 1], [0, 0, 0]], [[1, 2, 1], [0, 0, 0], [0, 0, 0]]]
 time_name={1:'공강시간1', 2:'1차자습시간', 3:'공강시간2', 4:'1, 2차 자습시간', 5:'공강시간3', 6:'평일자습시간', 7:'주말자습시간', 8:'밤샘시간'}
 reward_name=['자유로운 공강', '행복한 취미생활', '편안한 숙면']
 hp_list=[8, 11, 14, 17, 20]
@@ -102,7 +122,7 @@ hp_list=[8, 11, 14, 17, 20]
 for i in range(3):
     tmp=[]
     for j in range(5):
-        key = random.randint(5)
+        key = random.randint(1, 5)
         tmp.append(time_monster(task_name_all[i][j], reward_list[(key+j)%5], int(hp_list[(key+j)%5]*num_player/5)))
     task_list[task_header[i]]=tmp
 
@@ -114,10 +134,13 @@ stage_num=3
 round_num=5
 
 while True:
+    for j in player_list:
+        j.init_reward()
     for i in range(stage_num):
         task_tmp=copy.deepcopy(task_list)
         run_game(num_player, round_num, task_tmp[task_header[i]])
-
+        for j in player_list:
+            j.init_time()
     if play_again():
         continue
     else:
