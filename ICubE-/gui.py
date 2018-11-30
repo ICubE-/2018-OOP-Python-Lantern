58d478ad7b0ffc6ad96a3db726530b10ffe58fd2
ICubE- <jho327@naver.com>
2018-11-30 22:45:47 +0900

    Update gui.py, client.py, server.py

--- ICubE-/gui.py
+++ ICubE-/gui.py
@@ -1,7 +1,9 @@
 import pygame as pg
-import pygame_textinput
+from . import pygame_textinput
 
 pg.init()
+
+FONT_DIR = '..\\SeeWhy\\font\\NanumGothic.ttf'
 
 TIME_DELAY = 10
 
@@ -31,9 +33,9 @@ class BaseGui:
         return input, input_box
 
 
-class ChooseNickname(BaseGui):
-    def __init__(self, width=640, height=480, font='..\\SeeWhy\\font\\NanumGothic.ttf', font_size=20):
-        BaseGui.__init__(self)
+class AlertConnectionError(BaseGui):
+    def __init__(self, width=200, height=100, font=FONT_DIR, font_size=20):
+        super().__init__()
         self.width = width
         self.height = height
         self.font = font
@@ -41,9 +43,24 @@ class ChooseNickname(BaseGui):
 
         self.screen = pg.display.set_mode((self.width, self.height))
         self.screen.fill(LIGHT_GRAY)
+        self.msg = "연결이 비정상적으로 종료되었습니다."
+        #self.btn =
+
+
+class ChooseNickname(BaseGui):
+    def __init__(self, width=640, height=480, font=FONT_DIR, font_size=20, banned_letters="$"):
+        super().__init__()
+        self.width = width
+        self.height = height
+        self.font = font
+        self.font_size = font_size
+        self.banned_letters = banned_letters
+
+        self.screen = pg.display.set_mode((self.width, self.height))
+        self.screen.fill(LIGHT_GRAY)
         self.desc = "사용할 이름을 입력하세요."
         self.text, self.text_rect = self.make_text(self.desc, self.font, self.font_size, BLACK, (self.width/2, 200))
-        self.input, self.input_box = self.make_input(font, font_size, BLACK, (self.width/2, 300), 200, 10)
+        self.input, self.input_box = self.make_input(font, font_size, BLACK, (self.width/2, 280), 200, 10)
 
     def show(self):
         while True:
@@ -68,63 +85,3 @@ class ChooseNickname(BaseGui):
 if __name__ == '__main__':
     tmp = ChooseNickname()
     tmp.show()
-
-"""
-import pygame as pg
-
-
-def main():
-    screen = pg.display.set_mode((640, 480))
-    font = pg.font.Font(None, 32)
-    clock = pg.time.Clock()
-    input_box = pg.Rect(100, 100, 140, 32)
-    color_inactive = pg.Color('lightskyblue3')
-    color_active = pg.Color('dodgerblue2')
-    color = color_inactive
-    active = False
-    text = ''
-    done = False
-
-    while not done:
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
-                done = True
-            if event.type == pg.MOUSEBUTTONDOWN:
-                # If the user clicked on the input_box rect.
-                if input_box.collidepoint(event.pos):
-                    # Toggle the active variable.
-                    active = not active
-                else:
-                    active = False
-                # Change the current color of the input box.
-                color = color_active if active else color_inactive
-            if event.type == pg.KEYDOWN:
-                if active:
-                    if event.key == pg.K_RETURN:
-                        print(text)
-                        text = ''
-                    elif event.key == pg.K_BACKSPACE:
-                        text = text[:-1]
-                    else:
-                        text += event.unicode
-
-        screen.fill((30, 30, 30))
-        # Render the current text.
-        txt_surface = font.render(text, True, color)
-        # Resize the box if the text is too long.
-        width = max(200, txt_surface.get_width()+10)
-        input_box.w = width
-        # Blit the text.
-        screen.blit(txt_surface, (input_box.x+5, input_box.y+5))
-        # Blit the input_box rect.
-        pg.draw.rect(screen, color, input_box, 2)
-
-        pg.display.flip()
-        clock.tick(30)
-
-
-if __name__ == '__main__':
-    pg.init()
-    main()
-    pg.quit()
-"""
